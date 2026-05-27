78cfac47ff35087d23b4da0878ee135b62ce1181
MaykoSilvestre <132282371+MaykoSilvestre@users.noreply.github.com>
2026-05-26 21:20:43 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -8,15 +8,27 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+
   );
+
 });
 
 self.addEventListener("fetch", event => {
+
   event.respondWith(
+
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
   );
+
 });
